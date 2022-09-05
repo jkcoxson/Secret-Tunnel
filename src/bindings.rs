8f622a1bf76955938ed15fb28f85a063dcd85940
Jackson Coxson <jkcoxson@gmail.com>
2022-09-05 15:34:56 -0600

    Add binding for minimuxer debug app

--- src/bindings.rs
+++ src/bindings.rs
@@ -297,3 +297,23 @@ pub extern "C" fn init_logger(level: libc::c_uint) -> libc::c_int {
 pub unsafe extern "C" fn start_usbmuxd(pairing_file: *mut libc::c_char) -> libc::c_int {
     minimuxer::minimuxer_c_start(pairing_file)
 }
+
+#[no_mangle]
+/// Debugs an app from an app ID
+/// # Safety
+/// Don't be stupid
+pub unsafe extern "C" fn minimuxer_debug_app(app_id: *mut libc::c_char) -> libc::c_int {
+    if app_id.is_null() {
+        return -1;
+    }
+
+    let c_str = std::ffi::CStr::from_ptr(app_id);
+
+    let app_id = match c_str.to_str() {
+        Ok(s) => s,
+        Err(_) => return -1,
+    }
+    .to_string();
+
+    minimuxer::tools::enable_jit(app_id)
+}
